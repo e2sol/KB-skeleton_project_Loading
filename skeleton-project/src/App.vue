14e41d55242efb3c406d1c868784a0504f54ac75
강예찬 <qkdvo1530@gmail.com>
2026-04-07 18:14:04 +0900

    Feat. 로그인 패스워드 입력

--- skeleton-project/src/App.vue
+++ skeleton-project/src/App.vue
@@ -1,10 +1,15 @@
-<script setup></script>
+<script setup>
+import BeforeLogin from "@/components/BeforeLogin.vue";
+
+const Login = BeforeLogin;
+</script>
 
 <template>
   <h1>한혜지 test</h1>
   <h1>안녕 나는 강예찬><</h1>
   <h1>장진서 0_<</h1>
   <h2>반가웡</h2>
+  <Login />
 </template>
 
 <style scoped></style>
--- skeleton-project/src/components/BeforeLogin.vue
+++ skeleton-project/src/components/BeforeLogin.vue
@@ -1,4 +1,25 @@
 <template>
-  <div></div>
+  <div>
+    <label for="inputPassword5" class="form-label" `>ID</label>
+    <input
+      type="text"
+      id="inputPassword5"
+      class="form-control"
+      aria-describedby="passwordHelpBlock"
+      placeholder="ID"
+    />
+  </div>
+  <label for="inputPassword5" class="form-label">Password</label>
+  <input
+    type="password"
+    id="inputPassword5"
+    class="form-control"
+    aria-describedby="passwordHelpBlock"
+    placeholder="password"
+  />
+  <div id="passwordHelpBlock" class="form-text">
+    비밀번호는 8자 이상으로 설정해주세요.
+  </div>
+  <button @click="">log in</button>
 </template>
 <script setup></script>
